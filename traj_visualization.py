fa5b67bb9c17f92896797332f60ba51c1a484827
Yushuo Du <yushuo.du@student.adelaide.edu.au>
2023-05-11 13:07:34 +0930

    加了控制

--- traj_visualization.py
+++ traj_visualization.py
@@ -4,41 +4,30 @@ from scipy.interpolate import CubicSpline
 import numpy as np
 import os
 import habitat_sim
-from habitat_sim.utils.common import quat_from_coeffs, quat_from_two_vectors, quat_rotate_vector
+from habitat_sim.utils.common import quat_from_coeffs
 import cv2
 from scipy.spatial.transform import Rotation, Slerp
 import scipy.interpolate
 
 
-def get_floor_level(y, floor_height_ranges):
-    for i, (min_height, max_height) in enumerate(floor_height_ranges):
-        if min_height <= y < max_height:
-            return i
+def parse_house_file(file_path):
+    floor_heights = {}
+    with open(file_path, "r") as file:
+        for line in file:
+            if line.startswith("L"):
+                data = line.strip().split()
+                floor_number = int(data[1])
+                min_height = float(data[5])
+                max_height = float(data[11])
+                floor_heights[floor_number] = (min_height, max_height)
+    return floor_heights
+
+
+def determine_floor(height, floor_heights):
+    for floor, (min_h, max_h) in floor_heights.items():
+        if min_h <= height < max_h:
+            return floor
     return None
-
-
-def parse_house_file(house_file):
-    with open(house_file, 'r') as f:
-        lines = f.readlines()
-
-    floor_height_ranges = []
-    for line in lines:
-        if line.startswith("L"):
-            tokens = line.strip().split()
-            floor_min_height = float(tokens[5])
-            floor_max_height = float(tokens[11])
-            floor_height_ranges.append((floor_min_height, floor_max_height))
-    print(f"floor height ranges: {floor_height_ranges}")
-    return floor_height_ranges
-
-
-def world_to_map_coordinates(world_coord, map_size, nav_bounds_min, nav_bounds_max):
-    map_scale = (
-        map_size[1] / (nav_bounds_max[2] - nav_bounds_min[2]), map_size[0] / (nav_bounds_max[0] - nav_bounds_min[0]))
-    map_coord = (
-        (world_coord[0] - nav_bounds_min[0]) * map_scale[1], (world_coord[2] - nav_bounds_min[2]) * map_scale[0])
-    map_coord = (int(round(map_coord[0])), int(round(map_coord[1])))
-    return map_coord
 
 
 def catmull_rom_spline(points, num_interpolated_points):
@@ -49,6 +38,15 @@ def catmull_rom_spline(points, num_interpolated_points):
     tck_y = CubicSpline(np.arange(n), points[:, 1], bc_type='clamped')
     tck_z = CubicSpline(np.arange(n), points[:, 2], bc_type='clamped')
     return np.column_stack((tck_x(t), tck_y(t), tck_z(t)))
+
+
+def world_to_map_coordinates(world_coord, map_size, nav_bounds_min, nav_bounds_max):
+    map_scale = (
+        map_size[1] / (nav_bounds_max[2] - nav_bounds_min[2]), map_size[0] / (nav_bounds_max[0] - nav_bounds_min[0]))
+    map_coord = (
+        (world_coord[0] - nav_bounds_min[0]) * map_scale[1], (world_coord[2] - nav_bounds_min[2]) * map_scale[0])
+    map_coord = (int(round(map_coord[0])), int(round(map_coord[1])))
+    return map_coord
 
 
 def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
@@ -73,9 +71,12 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
         filename_without_ext = os.path.splitext(val_episode["scene_id"])[0]
         navmesh_file = os.path.join(scene_directory, filename_without_ext + ".navmesh")
         house_file = os.path.join(scene_directory, filename_without_ext + ".house")
-        floor_height_ranges = parse_house_file(house_file)
+        floor_heights = parse_house_file(house_file)
         print(f"Navmesh file path: {navmesh_file}")
         print(f"House file path: {house_file}")
+
+        # getpath
+        path = [step["position"] for step in pred_episode_steps if not step["stop"]]
 
         # create simulator
         sim_settings = {
@@ -109,14 +110,56 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
         cfg = habitat_sim.Configuration(backend_cfg, [agent_cfg, agent_cfg])
         sim = habitat_sim.Simulator(cfg)
 
+        sim.pathfinder.load_nav_mesh(navmesh_file)
+        # Gets the boundaries of the navigation grid
         nav_bounds_min, nav_bounds_max = sim.pathfinder.get_bounds()
-        # Calculate map size
-        map_height = 1024
+
+        map_height = 640
         map_width = int(
             map_height * ((nav_bounds_max[0] - nav_bounds_min[0]) / (nav_bounds_max[2] - nav_bounds_min[2])))
         map_size = (map_width, map_height)
 
-        # 计算场景的中心
+        # create a blank 2d map
+        blank_map = np.zeros((map_size[1], map_size[0], 3), dtype=np.uint8)
+
+        floor_colors = {
+            0: (255, 0, 0),  # Red
+            1: (0, 255, 0),  # Green
+            2: (0, 0, 255)  # Blue
+        }
+
+        height_samples = 10  # Number of height samples to take within the range
+
+        for i in range(0, map_size[0], 1):
+            for j in range(0, map_size[1], 1):
+                world_coord_base = np.array(
+                    [nav_bounds_min[0] + (i / map_size[0]) * (nav_bounds_max[0] - nav_bounds_min[0]),
+                     0,
+                     nav_bounds_min[2] + (j / map_size[1]) * (nav_bounds_max[2] - nav_bounds_min[2])])
+                x, y = world_to_map_coordinates(world_coord_base, map_size, nav_bounds_min, nav_bounds_max)
+
+                navigable = False
+
+                floor = determine_floor(world_coord_base[1], floor_heights)
+                if floor is not None:
+                    min_height, max_height = floor_heights[floor]
+                    for h in np.linspace(min_height, max_height, height_samples):
+                        world_coord = world_coord_base + np.array([0, h, 0])
+                        if sim.pathfinder.is_navigable(world_coord):
+                            navigable = True
+                            break
+
+                if navigable:
+                    color = floor_colors.get(floor, (0, 0, 0))
+                    blank_map[y, x] = color
+                else:
+                    blank_map[y, x] = 255
+
+        for step in path:
+            x, y = world_to_map_coordinates(step, map_size, nav_bounds_min, nav_bounds_max)
+            cv2.circle(blank_map, (x, y), 2, (0, 0, 255), -1)
+
+        # set up 3d top down view
         center_x = 0
         center_y = 2.5
         center_z = 0
@@ -133,32 +176,20 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
 
         sim.close()
         sim = habitat_sim.Simulator(cfg)
+
         # Set up the color sensor for the first agent
         sim.config.agents[0].sensor_specifications = [color_sensor_spec]
 
         # Set up the topdown sensor for the second agent
         sim.config.agents[1].sensor_specifications = [topdown_sensor_spec]
 
-        # observations = sim.get_sensor_observations()
-        # topdown_observation = observations["topdown_sensor"]
-        # cv2.namedWindow("Top Down View", cv2.WINDOW_NORMAL)
-        # cv2.imshow("Top Down View", topdown_observation)
-        # cv2.resizeWindow("Top Down View", map_size[0], map_size[1])
-
-        # 3D Path
-        # setting camera angle
         agent_state = habitat_sim.AgentState()
         agent = sim.initialize_agent(sim_settings["default_agent"], agent_state)
-
-        # getpath
-        path = [step["position"] for step in pred_episode_steps if not step["stop"]]
-
+        agent_state = habitat_sim.AgentState()
+        agent.set_state(agent_state, reset_sensors=True)
         if not path:
             continue
         num_interpolated_points = 600
-
-        agent_state = habitat_sim.AgentState()
-        agent.set_state(agent_state, reset_sensors=True)
         # Move the camera along the path and capture the image
         rotations = []
         skipped_indices = []
@@ -196,41 +227,75 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
         slerp = Slerp(np.linspace(0, len(path) - 1, len(path)), Rotation.from_quat(rotations))
         interpolated_rotations = slerp(np.linspace(0, len(path) - 1, num_interpolated_points)).as_quat()
 
-        topdown_images = np.zeros((map_size[1], map_size[0], 3), dtype=np.uint8)
-        for i, point in enumerate(interpolated_path[:-1]):
-            agent_state = habitat_sim.AgentState()
-            agent_state.position = point
-            agent_state.rotation = quat_from_coeffs(interpolated_rotations[i])
+        rotation_increment = np.pi / 18  # Rotate by 10 degrees at a time
+        agent_state = habitat_sim.AgentState()
+        agent_state.position = interpolated_path[0]
+        agent_state.rotation = quat_from_coeffs(interpolated_rotations[0])
+
+        paused = False
+        i = 0
+        while i < len(interpolated_path) - 1:
+            # Check for keyboard input
+            key = cv2.waitKey(40) & 0xFF
+            if key == ord(' '):  # Space bar to pause
+                paused = not paused
+            elif key == ord('r'):
+                paused = False
+            elif paused:  # Only allow movement and rotation while paused
+                if key == ord('w'):
+                    i = min(i + 1, len(interpolated_path) - 1)
+                    agent_state.position = interpolated_path[i]
+                elif key == ord('s'):
+                    i = max(i - 1, 0)
+                    agent_state.position = interpolated_path[i]
+                elif key == ord('a'):
+                    y_rotation = habitat_sim.utils.common.quat_from_angle_axis(rotation_increment, np.array([0, 1, 0]))
+                    agent_state.rotation = y_rotation * agent_state.rotation
+                elif key == ord('d'):
+                    y_rotation = habitat_sim.utils.common.quat_from_angle_axis(-rotation_increment, np.array([0, 1, 0]))
+                    agent_state.rotation = y_rotation * agent_state.rotation
+            else:
+                i += 1
+                agent_state.position = interpolated_path[i]
+                agent_state.rotation = quat_from_coeffs(interpolated_rotations[i])
+
             sim.agents[0].set_state(agent_state, reset_sensors=True)
 
-            # Update the second agent (topdown camera) state
-            agent_state = habitat_sim.AgentState()
-            agent_state.position = point
-            agent_state.rotation = habitat_sim.utils.common.quat_from_angle_axis(-np.pi / 2, np.array([1, 0, 0]))
-            sim.agents[1].set_state(agent_state, reset_sensors=True)
+            # Update the second agent (top-down camera) state
+            agent_state_top_down = habitat_sim.AgentState()
+            agent_state_top_down.position = agent_state.position
+            agent_state_top_down.rotation = habitat_sim.utils.common.quat_from_angle_axis(-np.pi / 2,
+                                                                                          np.array([1, 0, 0]))
+            sim.agents[1].set_state(agent_state_top_down, reset_sensors=True)
 
+            # Capture and display the image
             observations = sim.get_sensor_observations()
-            rgb_observation = observations["color_sensor"]
             topdown_observation = observations["topdown_sensor"]
             topdown_observation = cv2.cvtColor(topdown_observation, cv2.COLOR_RGB2BGR)
+            rgb_observation = observations["color_sensor"]
+            rgb_observation = cv2.cvtColor(rgb_observation, cv2.COLOR_RGB2BGR)
 
-            # topdown_sensor_rotation = [-np.pi / 2, 0, 0]
-            # sim._sensors["topdown_sensor"].rotation = topdown_sensor_rotation
+            x, y = world_to_map_coordinates(agent_state.position, map_size, nav_bounds_min, nav_bounds_max)
+            cv2.circle(blank_map, (x, y), 1, (0, 255, 0), -1)
 
-            # Get sensor observations
+            resized_map = cv2.resize(blank_map, (map_size[0] * 2, map_size[1] * 2), interpolation=cv2.INTER_AREA)
 
-            cv2.imshow("RGB", rgb_observation)
+            width = max(rgb_observation.shape[1], resized_map.shape[1], topdown_observation.shape[1])
 
-            # Get sensor observations for the top-down camera
+            rgb_observation = cv2.resize(rgb_observation,
+                                         (width, int(rgb_observation.shape[0] * width / rgb_observation.shape[1])))
+            resized_map = cv2.resize(resized_map, (width, int(resized_map.shape[0] * width / resized_map.shape[1])))
+            topdown_observation = cv2.resize(topdown_observation, (
+                width, int(topdown_observation.shape[0] * width / topdown_observation.shape[1])))
 
-            alpha = 0.5  # 你可以根据需要调整这个值，它表示新画面的透明度，范围是 0 到 1
-            # combined_map = cv2.add(combined_map, topdown_observation)
+            # Then, vertically stack the images
+            combined_image = np.vstack((rgb_observation, topdown_observation, resized_map))
 
-            cv2.namedWindow("Top Down View", cv2.WINDOW_NORMAL)
-            cv2.imshow("Top Down View", topdown_observation)
+            # Now you can display the combined image
+            cv2.imshow("Combined Image", combined_image)
 
-            cv2.waitKey(75)
-
+            if cv2.waitKey(40) & 0xFF == ord('q'):
+                break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
