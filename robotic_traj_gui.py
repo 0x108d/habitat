c90ca5a6474eb6ecb49c7f072fc0ae9df5ff14ca
Yushuo Du <yushuo.du@student.adelaide.edu.au>
2023-06-06 00:34:13 +0930

    优化拆分函数

---
+++ robotic_traj_gui.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter import filedialog, ttk
+import json
+
+from traj_visualization import visualize_trajectory
+
+
+class GUI(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.pred_json_file = None
+        self.val_seen_json_file = None
+        self.scene_directory = None
+        self.episode_id = tk.StringVar()
+
+        self.button_pred_json_file = tk.Button(self, text="Select pred_json_file", command=self.load_pred_json_file)
+        self.button_pred_json_file.pack()
+
+        self.button_val_seen_json_file = tk.Button(self, text="Select val_seen_json_file",
+                                                   command=self.load_val_seen_json_file)
+        self.button_val_seen_json_file.pack()
+
+        self.button_scene_directory = tk.Button(self, text="Select Scene Directory", command=self.load_scene_directory)
+        self.button_scene_directory.pack()
+
+        self.combo_episode_id = ttk.Combobox(self, textvariable=self.episode_id)
+        self.combo_episode_id.pack()
+
+        self.button_start = tk.Button(self, text="Start", command=self.start)
+        self.button_start.pack()
+
+    def load_pred_json_file(self):
+        self.pred_json_file = filedialog.askopenfilename()
+        # Update episode id choices based on the selected pred_json_file
+        with open(self.pred_json_file, 'r') as f:
+            pred_data = json.load(f)
+        self.combo_episode_id['values'] = list(pred_data.keys())
+
+    def load_val_seen_json_file(self):
+        self.val_seen_json_file = filedialog.askopenfilename()
+
+    def load_scene_directory(self):
+        self.scene_directory = filedialog.askdirectory()
+
+    def start(self):
+        selected_episode_id = self.episode_id.get()
+        visualize_trajectory(self.pred_json_file, self.val_seen_json_file, self.scene_directory, selected_episode_id)
+
+
+gui = GUI()
+gui.mainloop()
--- traj_visualization.py
+++ traj_visualization.py
@@ -22,6 +22,7 @@ def parse_house_file(file_path):
                 floor_heights[floor_number] = (min_height, max_height)
     return floor_heights
 
+
 def determine_floor(height, floor_heights):
     for floor, (min_h, max_h) in floor_heights.items():
         if min_h <= height < max_h:
@@ -48,7 +49,7 @@ def world_to_map_coordinates(world_coord, map_size, nav_bounds_min, nav_bounds_m
     return map_coord
 
 
-def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
+def load_data(pred_json_file, val_seen_json_file, selected_episode_id):
     # loading JSON file
     with open(pred_json_file, 'r') as f:
         pred_data = json.load(f)
@@ -56,22 +57,226 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
     with open(val_seen_json_file, 'r') as f:
         val_seen_data = json.load(f)
 
-    for pred_episode_id, pred_episode_steps in pred_data.items():
-        val_episode = None
-        for episode in val_seen_data["episodes"]:
-            if str(episode["episode_id"]) == pred_episode_id:
-                val_episode = episode
+    if selected_episode_id not in pred_data:
+        print(f"Episode {selected_episode_id} not found in pred.json")
+        return None, None, None
 
-                print(f"Episode ID: {pred_episode_id}")
+    pred_episode_steps = pred_data[selected_episode_id]
+    val_episode = None
+    for episode in val_seen_data["episodes"]:
+        if str(episode["episode_id"]) == selected_episode_id:
+            val_episode = episode
+            break
 
-                break
+    if val_episode is None:
+        print(f"Episode {selected_episode_id} not found in val_seen.json")
+        return None, None, None
 
-        if val_episode is None:
-            print(f"Episode {pred_episode_id} not found in val_seen.json")
+    return pred_episode_steps, val_episode, val_seen_data
+
+
+def create_simulator(scene_file, sensor_height, sensor_width, width, height, center_coordinates):
+    # create simulator
+    sim_settings = {
+        "scene": scene_file,
+        "default_agent": 0,
+        "sensor_height": sensor_height,
+        "sensor_width": sensor_width,
+        "width": width,
+        "height": height,
+        "center_coordinates": center_coordinates
+    }
+    backend_cfg = habitat_sim.SimulatorConfiguration()
+    backend_cfg.scene_id = sim_settings["scene"]
+    backend_cfg.enable_physics = False
+
+    # Set up the color sensor
+    sensor_cfg = habitat_sim.CameraSensorSpec()
+    sensor_cfg.resolution = [sim_settings["height"], sim_settings["width"]]
+    sensor_cfg.position = [0, sim_settings["sensor_height"], 0]
+    sensor_cfg.sensor_type = habitat_sim.SensorType.COLOR
+    sensor_cfg.uuid = "color_sensor"
+
+    color_sensor_spec = habitat_sim.CameraSensorSpec()
+    color_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    color_sensor_spec.hfov = np.pi / 3.5
+    color_sensor_spec.vfov = np.pi * 45
+    color_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+    color_sensor_spec.channels_first = True
+    color_sensor_spec.encoding = "png"
+
+    # Set up 3D top-down view
+    center_x, center_y, center_z = sim_settings["center_coordinates"]
+    topdown_sensor_spec = habitat_sim.CameraSensorSpec()
+    topdown_sensor_spec.uuid = "topdown_sensor"
+    topdown_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
+    topdown_sensor_spec.resolution = [1280, 1080]
+    topdown_sensor_spec.position = [center_x, center_y, center_z]
+    topdown_sensor_spec.orientation = [-np.pi / 2.5, 0, 0]
+    topdown_sensor_spec.vfov = np.pi * 45
+    topdown_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
+
+    agent_cfg = habitat_sim.agent.AgentConfiguration()
+    agent_cfg.sensor_specifications = [sensor_cfg, topdown_sensor_spec]
+
+    cfg = habitat_sim.Configuration(backend_cfg, [agent_cfg, agent_cfg])
+    sim = habitat_sim.Simulator(cfg)
+
+    # Set up the color sensor for the first agent
+    sim.config.agents[0].sensor_specifications = [color_sensor_spec]
+
+    # Set up the topdown sensor for the second agent
+    sim.config.agents[1].sensor_specifications = [topdown_sensor_spec]
+
+    agent_state = habitat_sim.AgentState()
+    agent = sim.initialize_agent(sim_settings["default_agent"], agent_state)
+    agent_state = habitat_sim.AgentState()
+    agent.set_state(agent_state, reset_sensors=True)
+
+    return sim
+
+
+def create_navmesh_map(sim, navmesh_file, map_height, floor_heights, height_samples=10):
+    sim.pathfinder.load_nav_mesh(navmesh_file)
+    nav_bounds_min, nav_bounds_max = sim.pathfinder.get_bounds()
+
+    map_width = int(map_height * ((nav_bounds_max[0] - nav_bounds_min[0]) / (nav_bounds_max[2] - nav_bounds_min[2])))
+    map_size = (map_width, map_height)
+
+    blank_map = np.zeros((map_size[1], map_size[0], 3), dtype=np.uint8)
+
+    floor_colors = {
+        0: (255, 0, 0),  # Red
+        1: (0, 255, 0),  # Green
+        2: (0, 0, 255)  # Blue
+    }
+
+    for i in range(0, map_size[0], 1):
+        for j in range(0, map_size[1], 1):
+            world_coord_base = np.array(
+                [nav_bounds_min[0] + (i / map_size[0]) * (nav_bounds_max[0] - nav_bounds_min[0]),
+                 0,
+                 nav_bounds_min[2] + (j / map_size[1]) * (nav_bounds_max[2] - nav_bounds_min[2])])
+            x, y = world_to_map_coordinates(world_coord_base, map_size, nav_bounds_min, nav_bounds_max)
+
+            navigable = False
+
+            floor = determine_floor(world_coord_base[1], floor_heights)
+            if floor is not None:
+                min_height, max_height = floor_heights[floor]
+                for h in np.linspace(min_height, max_height, height_samples):
+                    world_coord = world_coord_base + np.array([0, h, 0])
+                    if sim.pathfinder.is_navigable(world_coord):
+                        navigable = True
+                        break
+
+            if navigable:
+                color = floor_colors.get(floor, (0, 0, 0))
+                blank_map[y, x] = color
+            else:
+                blank_map[y, x] = 255
+
+    return blank_map, map_size, nav_bounds_min, nav_bounds_max
+
+
+def draw_path_on_map(blank_map, path, map_size, nav_bounds_min, nav_bounds_max):
+    for step in path:
+        print('Step:', step)
+        x, _, z = step
+        x, y = world_to_map_coordinates(step, map_size, nav_bounds_min, nav_bounds_max)
+        cv2.circle(blank_map, (x, y), 5, (0, 0, 255), -1)
+
+
+def compute_path_rotations(path):
+    num_interpolated_points = 600
+    rotations = []
+    skipped_indices = []
+
+    for i, point in enumerate(path[:-1]):
+        point = np.array(point)
+        next_point = np.array(path[i + 1])
+        direction_vector = next_point - point
+        direction_norm = np.linalg.norm(direction_vector)
+
+        # If the norm of the direction vector is too small, skip this point
+        if direction_norm < 1e-6:
+            skipped_indices.append(i)
             continue
+
+        direction_vector /= direction_norm
+        # Calculate camera orientation (quaternion)
+        up_vector = np.array([0, 1, 0])
+        right_vector = np.cross(direction_vector, up_vector)
+        right_vector /= np.linalg.norm(right_vector)
+        up_vector = np.cross(right_vector, direction_vector)
+
+        rotation_matrix = np.stack([right_vector, up_vector, -direction_vector], axis=1)
+        rotation = Rotation.from_matrix(rotation_matrix)
+        quaternion = rotation.as_quat()
+        rotations.append(quaternion)
+
+    return rotations, skipped_indices
+
+
+def calculate_rotations(path):
+    rotations = []
+    skipped_indices = []
+    for i, point in enumerate(path[:-1]):
+        point = np.array(point)
+        next_point = np.array(path[i + 1])
+        direction_vector = next_point - point
+        direction_norm = np.linalg.norm(direction_vector)
+
+        # If the norm of the direction vector is too small, skip this point
+        if direction_norm < 1e-6:
+            skipped_indices.append(i)
+            continue
+
+        direction_vector /= direction_norm
+        # Calculate camera orientation (quaternion)
+        up_vector = np.array([0, 1, 0])
+        right_vector = np.cross(direction_vector, up_vector)
+        right_vector /= np.linalg.norm(right_vector)
+        up_vector = np.cross(right_vector, direction_vector)
+
+        rotation_matrix = np.stack([right_vector, up_vector, -direction_vector], axis=1)
+        rotation = Rotation.from_matrix(rotation_matrix)
+        quaternion = rotation.as_quat()
+        rotations.append(quaternion)
+
+    # Add the last point's rotation
+    rotations.append(rotations[-1])
+    return rotations, skipped_indices
+
+
+def remove_skipped_indices(path, skipped_indices):
+    for index in reversed(skipped_indices):
+        del path[index]
+    return path
+
+
+def interpolate_path(path, num_interpolated_points):
+    interpolator = scipy.interpolate.interp1d(np.arange(len(path)), path, axis=0)
+    interpolated_path = interpolator(np.linspace(0, len(path) - 1, num_interpolated_points))
+    return interpolated_path
+
+
+def interpolate_rotations(rotations, num_interpolated_points):
+    slerp = Slerp(np.linspace(0, len(rotations) - 1, len(rotations)), Rotation.from_quat(rotations))
+    interpolated_rotations = slerp(np.linspace(0, len(rotations) - 1, num_interpolated_points)).as_quat()
+    return interpolated_rotations
+
+
+def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory, selected_episode_id):
+    # loading JSON file
+    pred_episode_steps, val_episode, val_seen_data = load_data(pred_json_file, val_seen_json_file, selected_episode_id)
+    if pred_episode_steps is None or val_episode is None or val_seen_data is None:
+        return
+    else:
         reference_path = val_episode["reference_path"]
         print(f"Reference Path: {reference_path}")
-        scene_file = os.path.join(scene_directory, val_episode["scene_id"])
+        instruction_text = val_episode["instruction"]
+        print(f"Instruction Text: {instruction_text}")
         filename_without_ext = os.path.splitext(val_episode["scene_id"])[0]
         navmesh_file = os.path.join(scene_directory, filename_without_ext + ".navmesh")
         house_file = os.path.join(scene_directory, filename_without_ext + ".house")
@@ -81,157 +286,33 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
 
         # getpath
         path = [step["position"] for step in pred_episode_steps if not step["stop"]]
+        # set up 3d and top down view
+        scene_file = os.path.join(scene_directory, val_episode["scene_id"])
+        sensor_height = 0.7
+        sensor_width = 1024
+        width = 1280
+        height = 640
+        center_coordinates = [0, 2.5, 0]
+        sim = create_simulator(scene_file, sensor_height, sensor_width, width, height, center_coordinates)
+        # set up 2d map
+        map_height = 640  # specify your desired map height
+        blank_map, map_size, nav_bounds_min, nav_bounds_max = create_navmesh_map(sim, navmesh_file, map_height,
+                                                                                 floor_heights)
+        # Draw the path on the map
+        draw_path_on_map(blank_map, reference_path, map_size, nav_bounds_min, nav_bounds_max)
 
-        # create simulator
-        sim_settings = {
-            "scene": scene_file,
-            "default_agent": 0,
-            "sensor_height": 0.7,
-            "sensor_width": 1024,
-            "width": 1280,
-            "height": 640,
-        }
-        backend_cfg = habitat_sim.SimulatorConfiguration()
-        backend_cfg.scene_id = sim_settings["scene"]
-        backend_cfg.enable_physics = False
-
-        sensor_cfg = habitat_sim.CameraSensorSpec()
-        sensor_cfg.resolution = [sim_settings["height"], sim_settings["width"]]
-        sensor_cfg.position = [0, sim_settings["sensor_height"], 0]
-        sensor_cfg.sensor_type = habitat_sim.SensorType.COLOR
-        sensor_cfg.uuid = "color_sensor"
-
-        color_sensor_spec = habitat_sim.CameraSensorSpec()
-        color_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
-        color_sensor_spec.hfov = np.pi / 3.5
-        color_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
-        color_sensor_spec.channels_first = True
-        color_sensor_spec.encoding = "png"
-
-        agent_cfg = habitat_sim.agent.AgentConfiguration()
-        agent_cfg.sensor_specifications = [sensor_cfg]
-
-        cfg = habitat_sim.Configuration(backend_cfg, [agent_cfg, agent_cfg])
-        sim = habitat_sim.Simulator(cfg)
-
-        sim.pathfinder.load_nav_mesh(navmesh_file)
-        # Gets the boundaries of the navigation grid
-        nav_bounds_min, nav_bounds_max = sim.pathfinder.get_bounds()
-
-        map_height = 640
-        map_width = int(
-            map_height * ((nav_bounds_max[0] - nav_bounds_min[0]) / (nav_bounds_max[2] - nav_bounds_min[2])))
-        map_size = (map_width, map_height)
-
-        # create a blank 2d map
-        blank_map = np.zeros((map_size[1], map_size[0], 3), dtype=np.uint8)
-
-        floor_colors = {
-            0: (255, 0, 0),  # Red
-            1: (0, 255, 0),  # Green
-            2: (0, 0, 255)  # Blue
-        }
-
-        height_samples = 10  # Number of height samples to take within the range
-
-        for i in range(0, map_size[0], 1):
-            for j in range(0, map_size[1], 1):
-                world_coord_base = np.array(
-                    [nav_bounds_min[0] + (i / map_size[0]) * (nav_bounds_max[0] - nav_bounds_min[0]),
-                     0,
-                     nav_bounds_min[2] + (j / map_size[1]) * (nav_bounds_max[2] - nav_bounds_min[2])])
-                x, y = world_to_map_coordinates(world_coord_base, map_size, nav_bounds_min, nav_bounds_max)
-
-                navigable = False
-
-                floor = determine_floor(world_coord_base[1], floor_heights)
-                if floor is not None:
-                    min_height, max_height = floor_heights[floor]
-                    for h in np.linspace(min_height, max_height, height_samples):
-                        world_coord = world_coord_base + np.array([0, h, 0])
-                        if sim.pathfinder.is_navigable(world_coord):
-                            navigable = True
-                            break
-
-                if navigable:
-                    color = floor_colors.get(floor, (0, 0, 0))
-                    blank_map[y, x] = color
-                else:
-                    blank_map[y, x] = 255
-        # for step in path:
-        for step in reference_path:
-            print('Step:', step)
-            x, _, z = step
-            x, y = world_to_map_coordinates(step, map_size, nav_bounds_min, nav_bounds_max)
-            cv2.circle(blank_map, (x, y), 5, (0, 0, 255), -1)
-
-        # set up 3d top down view
-        center_x = 0
-        center_y = 2.5
-        center_z = 0
-        topdown_sensor_spec = habitat_sim.CameraSensorSpec()
-        topdown_sensor_spec.uuid = "topdown_sensor"
-        topdown_sensor_spec.sensor_type = habitat_sim.SensorType.COLOR
-        topdown_sensor_spec.resolution = [1280, 1080]
-        topdown_sensor_spec.position = [center_x, center_y, center_z]
-
-        topdown_sensor_spec.orientation = [-np.pi / 2.5, 0, 0]
-        topdown_sensor_spec.vfov = np.pi * 45
-        topdown_sensor_spec.sensor_subtype = habitat_sim.SensorSubType.PINHOLE
-        agent_cfg.sensor_specifications = [sensor_cfg, topdown_sensor_spec]
-
-        sim.close()
-        sim = habitat_sim.Simulator(cfg)
-
-        # Set up the color sensor for the first agent
-        sim.config.agents[0].sensor_specifications = [color_sensor_spec]
-
-        # Set up the topdown sensor for the second agent
-        sim.config.agents[1].sensor_specifications = [topdown_sensor_spec]
-
-        agent_state = habitat_sim.AgentState()
-        agent = sim.initialize_agent(sim_settings["default_agent"], agent_state)
-        agent_state = habitat_sim.AgentState()
-        agent.set_state(agent_state, reset_sensors=True)
-        if not path:
-            continue
+        # if not path:
+        #     continue
         num_interpolated_points = 600
-        # Move the camera along the path and capture the image
-        rotations = []
-        skipped_indices = []
-        for i, point in enumerate(path[:-1]):
-            point = np.array(point)
-            next_point = np.array(path[i + 1])
-            direction_vector = next_point - point
-            direction_norm = np.linalg.norm(direction_vector)
+        # Calculate rotations and skipped indices
+        rotations, skipped_indices = calculate_rotations(path)
 
-            # If the norm of the direction vector is too small, skip this point
-            if direction_norm < 1e-6:
-                skipped_indices.append(i)
-                continue
+        # Remove skipped indices from the path
+        path = remove_skipped_indices(path, skipped_indices)
 
-            direction_vector /= direction_norm
-            # Calculate camera orientation (quaternion)
-            up_vector = np.array([0, 1, 0])
-            right_vector = np.cross(direction_vector, up_vector)
-            right_vector /= np.linalg.norm(right_vector)
-            up_vector = np.cross(right_vector, direction_vector)
-
-            rotation_matrix = np.stack([right_vector, up_vector, -direction_vector], axis=1)
-            rotation = Rotation.from_matrix(rotation_matrix)
-            quaternion = rotation.as_quat()
-            rotations.append(quaternion)
-
-        # Add the last point's rotation
-        rotations.append(rotations[-1])
-        for index in reversed(skipped_indices):
-            del path[index]
-        interpolator = scipy.interpolate.interp1d(np.arange(len(path)), path, axis=0)
-        interpolated_path = interpolator(np.linspace(0, len(path) - 1, num_interpolated_points))
-
-        # Interpolate rotations
-        slerp = Slerp(np.linspace(0, len(path) - 1, len(path)), Rotation.from_quat(rotations))
-        interpolated_rotations = slerp(np.linspace(0, len(path) - 1, num_interpolated_points)).as_quat()
+        # Interpolate the path and rotations
+        interpolated_path = interpolate_path(path, num_interpolated_points)
+        interpolated_rotations = interpolate_rotations(rotations, num_interpolated_points)
 
         rotation_increment = np.pi / 18  # Rotate by 10 degrees at a time
         agent_state = habitat_sim.AgentState()
@@ -304,11 +385,10 @@ def visualize_trajectory(pred_json_file, val_seen_json_file, scene_directory):
             if cv2.waitKey(40) & 0xFF == ord('q'):
                 break
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pred-json', type=str, required=True, help='Path to the pred_val_seen JSON file')
-    parser.add_argument('--val-json', type=str, required=True, help='Path to the JSON file')
-    parser.add_argument('--scene-dir', type=str, required=True, help='Path to the directory containing glb files')
-    args = parser.parse_args()
-    visualize_trajectory(args.pred_json, args.val_json, args.scene_dir)
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--pred-json', type=str, required=True, help='Path to the pred_val_seen JSON file')
+#     parser.add_argument('--val-json', type=str, required=True, help='Path to the JSON file')
+#     parser.add_argument('--scene-dir', type=str, required=True, help='Path to the directory containing glb files')
+#     args = parser.parse_args()
+#     visualize_trajectory(args.pred_json, args.val_json, args.scene_dir)
